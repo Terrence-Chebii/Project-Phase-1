dfb6198b77470ad1e1eca8f46cc7d52f3b29d746
bigman <terrence.chebii@student.moringaschool.com>
2023-04-05 21:58:06 +0300

    give the form a function

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-let namee = document.querySelector('input')
+let btn = document.querySelector('#button')
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    let animeName = anime.value
+    let form = document.querySelector('#searchForm')
+    form.reset()
 
+    fetch(`https://api.jikan.moe/v4/anime?q=${animeName}`)
+    .then(res=>res.json())
+    .then(data=>displayResults(data))
+})
 
-fetch(`https://api.jikan.moe/v4/anime?q=${namee}&sfw`)
-.then(res=>res.json())
-.then(data=>displayResulys(data))
-
-function displayResulys(data){
-    console.log(data)
+function displayResults(data){ 
+    let main = document.querySelector('div')
+    main.innerHTML = '' // clear previous results
+    data.data.forEach(anime => {
+        let content = document.createElement('section')
+        content.innerHTML = `
+        <div id='display'>
+           <button>✬</button>
+           <img src='${anime.images.jpg.image_url}'>
+           <h3>${anime.title}<h3>
+           </div>
+        `
+        console.log(content)
+        main.appendChild(content)
+    });
 }
+
